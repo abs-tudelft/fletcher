83ac03af823cecb8a5634c32019a23ff07bd2150
Johan Peltenburg <johan.peltenburg@gmail.com>
2018-02-20 13:47:34 -0600

    Initialize SNAP support

---
+++ runtime/include/fletcher/snap/snap.h
@@ -0,0 +1,70 @@
+// Copyright 2018 Delft University of Technology
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <cstdint>
+#include <vector>
+
+#include <arrow/api.h>
+
+#include "../common.h"
+#include "../FPGAPlatform.h"
+#include "../UserCore.h"
+
+struct snap_card;
+struct snap_action;
+struct snap_queue;
+
+namespace fletcher {
+
+/// \class SNAPPlatform
+/// \brief An implementation of an FPGAPlatform for SNAP
+class SNAPPlatform : public FPGAPlatform
+{
+ public:
+  ~SNAPPlatform();
+  /**
+   * \brief SNAPPlatform constructor
+   *
+   */
+  SNAPPlatform(int card_no=0, uint32_t action_type=0x00000001);
+
+  int write_mmio(uint64_t offset, fr_t value);
+  int read_mmio(uint64_t offset, fr_t* dest);
+
+  void set_alignment(uint64_t alignment);
+
+  bool good();
+
+ private:
+  std::string _name = "CAPI SNAP";
+  uint64_t alignment = 4096;  // TODO: this should become 64 after Arrow spec.
+  uint64_t organize_buffers(const std::vector<BufConfig> &source_buffers,
+                            std::vector<BufConfig> &dest_buffers);
+
+  bool error = false;
+
+  // Snap device path
+  char device[64];
+
+  // Snap card handle
+  struct snap_card *card_handle;
+
+  // Snap action handle
+  struct snap_action *action_handle = NULL;
+
+};
+
+}
---
+++ runtime/src/fletcher/snap/snap.cpp
@@ -0,0 +1,132 @@
+// Copyright 2018 Delft University of Technology
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <omp.h>
+#include <fstream>
+
+#include <arrow/api.h>
+
+#include "../../../include/fletcher.h"
+#include "../../../include/fletcher/logging.h"
+#include "../../../include/fletcher/snap/snap.h"
+
+extern "C" {
+#include "libsnap.h"
+}
+
+namespace fletcher {
+
+SNAPPlatform::SNAPPlatform(int card_no, uint32_t action_type)
+{
+  LOGD("Setting up SNAP platform.");
+
+  // Check psl_server.dat is present
+
+  std::ifstream f("pslse_server.dat");
+  if (!f.good()) {
+    LOGE("No pslse_server.dat file present in working directory. Entering error state.");
+    error = true;
+    return;
+  }
+
+  sprintf(device, "/dev/cxl/afu%d.0s", card_no);
+  card_handle = snap_card_alloc_dev(device, SNAP_VENDOR_ID_IBM, SNAP_DEVICE_ID_SNAP);
+
+  if (card_handle == NULL) {
+    LOGE("Could not allocate SNAP card. Entering error state.");
+    error = true;
+    return;
+  }
+
+  unsigned long ioctl_data;
+
+  snap_card_ioctl(card_handle, GET_CARD_TYPE, (unsigned long) &ioctl_data);
+
+  switch (ioctl_data) {
+    case 0:
+      LOGD("ADKU3");
+      break;
+    case 1:
+      LOGD("N250S");
+      break;
+    case 16:
+      LOGD("N250SP");
+      break;
+    default:
+      LOGD("Unknown");
+      break;
+  }
+
+  snap_card_ioctl(card_handle, GET_SDRAM_SIZE, (unsigned long) &ioctl_data);
+  LOGD("Available card RAM: " << (int)ioctl_data);
+
+  snap_action_flag_t attach_flags = (snap_action_flag_t) 0;
+
+  action_handle = snap_attach_action(card_handle, action_type, attach_flags, 100);
+}
+
+SNAPPlatform::~SNAPPlatform()
+{
+
+}
+
+uint64_t SNAPPlatform::organize_buffers(const std::vector<BufConfig> &source_buffers,
+                                        std::vector<BufConfig> &dest_buffers)
+{
+  uint64_t bytes = 0;
+
+  // Simply copy the source to the destination BufConfigs
+  // as in SNAP the FPGA can access the host memory
+  // using an address translation service.
+  for (auto const &src : source_buffers) {
+    bytes += src.size;
+    dest_buffers.push_back(src);
+  }
+
+  return bytes;
+}
+
+inline int SNAPPlatform::write_mmio(uint64_t offset, fr_t value)
+{
+  if (!error) {
+    snap_mmio_write64(card_handle, offset, value);
+    return FLETCHER_OK;
+  } else {
+    LOGE("Write unsuccessful. SNAP platform is in error state. Write " << STRHEX64 << value << " to " << STRHEX64 << offset);
+    return FLETCHER_ERROR;
+  }
+}
+
+inline int SNAPPlatform::read_mmio(uint64_t offset, fr_t* dest)
+{
+  fr_t ret = 0xDEADBEEFDEADBEEF;
+  if (!error) {
+    snap_mmio_read64(card_handle, offset, &ret);
+    return FLETCHER_OK;
+  } else {
+    LOGE("Read unsuccessful. SNAP platform is in error state. Read from " << STRHEX64 << offset);
+    return FLETCHER_ERROR;
+  }
+  *dest = ret;
+  return FLETCHER_OK;
+}
+
+bool SNAPPlatform::good()
+{
+  return !error;
+}
+
+}
