17887692bf3e9160a7861d6711651432a5884292
Unknown <j.w.peltenburg@tudelft.nl>
2018-10-16 14:36:16 +0200

    Update sum example

--- examples/sum/software/sum.cpp
+++ examples/sum/software/sum.cpp
@@ -33,36 +33,33 @@
 #include <arrow/api.h>
 
 // Fletcher
-#include <fletcher/fletcher.h>
+#include <fletcher/api.h>
 
-// Default to `echo' platform
-#ifndef PLATFORM
-  #define PLATFORM 0
-#endif
+using std::shared_ptr;
+using std::numeric_limits;
+using std::vector;
+using std::static_pointer_cast;
 
-using namespace std;
-
-typedef chrono::high_resolution_clock perf_clock;
+typedef std::chrono::high_resolution_clock perf_clock;
 
 intmax_t sum_check = 0;
 
 /**
  * Create an Arrow table containing one column of random 64-bit numbers.
  */
-shared_ptr<arrow::Table> create_table(int num_rows)
-{
+shared_ptr<arrow::RecordBatch> createRecordBatch(int num_rows) {
   // Set up random number generator
   std::mt19937 rng;
   rng.seed(0);
   // Ensure the sum fits in the return types used by all summing methods
-  int64_t element_max = min(
-    (uintmax_t) numeric_limits<int64_t>::max() / num_rows, // arrow data type,
-    min(
-      // return type on CPU
-      (uintmax_t) numeric_limits<long long int>::max() / num_rows,
-      // return type on FPGA
-      (uintmax_t) numeric_limits<fletcher::fa_t>::max() / num_rows
-    )
+  int64_t element_max = std::min(
+      (uintmax_t) numeric_limits<int64_t>::max() / num_rows, // arrow data type,
+      std::min(
+          // return type on CPU
+          (uintmax_t) numeric_limits<long long int>::max() / num_rows,
+          // return type on FPGA
+          (uintmax_t) numeric_limits<int64_t>::max() / num_rows
+      )
   );
   std::uniform_int_distribution<int64_t> int_dist(0, element_max);
 
@@ -78,146 +75,130 @@ shared_ptr<arrow::Table> create_table(int num_rows)
 
   // Define the schema
   auto column_field = arrow::field("weight", arrow::int64(), false);
-  vector<shared_ptr<arrow::Field>> fields = { column_field };
-  shared_ptr<arrow::Schema> schema = make_shared<arrow::Schema>(fields);
+  vector<shared_ptr<arrow::Field>> fields = {column_field};
+  shared_ptr<arrow::Schema> schema = std::make_shared<arrow::Schema>(fields);
 
   // Create an array and finish the builder
   shared_ptr<arrow::Array> num_array;
   int_builder.Finish(&num_array);
 
   // Create and return the table
-  return move(arrow::Table::Make(schema, { num_array }));
+  return std::move(arrow::RecordBatch::Make(schema, num_rows, {num_array}));
 }
-
 
 /**
  * Calculate the sum of all numbers in the arrow column using the CPU.
  */
-long long int arrow_column_sum_cpu(shared_ptr<arrow::Table> table)
-{
+int64_t sumCPU(const shared_ptr<arrow::RecordBatch> &recordbatch) {
   // Performance timer open
   auto t1 = perf_clock::now();
 
   // Get data pointer
-  auto num_array = static_pointer_cast<arrow::Int64Array>(
-                                        table->column(0)->data()->chunk(0) );
+  auto num_array = static_pointer_cast<arrow::Int64Array>(recordbatch->column(0));
   const int64_t *data = num_array->raw_values();
   // Sum loop
-  long long int sum = 0;
+  int64_t sum = 0;
   for (int i = 0; i < num_array->length(); i++) {
     sum += data[i];
   }
 
   // Performance timer close
   auto t2 = perf_clock::now();
-  chrono::duration<double> time_span = chrono::duration_cast<
-                                          chrono::duration<double> >(t2 - t1);
-  std::cout << "Sum CPU time: " << time_span.count() << " seconds" << endl;
+  std::chrono::duration<double> time_span = std::chrono::duration_cast<
+      std::chrono::duration<double> >(t2 - t1);
+  std::cout << "Sum CPU time: " << time_span.count() << " seconds" << std::endl;
   return sum;
 }
-
 
 /**
  * Calculate the sum of all numbers in the arrow column using an FPGA.
  */
-fletcher::fa_t arrow_column_sum_fpga(shared_ptr<arrow::Table> table)
-{
+int64_t sumFPGA(const shared_ptr<arrow::RecordBatch> &recordbatch) {
+  std::shared_ptr<fletcher::Platform> platform;
+  std::shared_ptr<fletcher::Context> context;
+
   // Create a platform
-#if(PLATFORM == 0)
-  shared_ptr<fletcher::EchoPlatform> platform(new fletcher::EchoPlatform());
-#elif(PLATFORM == 1)
-  shared_ptr<fletcher::AWSPlatform> platform(new fletcher::AWSPlatform());
-#elif(PLATFORM == 2)
-  shared_ptr<fletcher::SNAPPlatform> platform(new fletcher::SNAPPlatform());
-#else
-  #error "PLATFORM must be 0, 1 or 2"
-#endif
+  fletcher::Platform::create(&platform).ewf();
+
+  // Create a context
+  fletcher::Context::Make(&context, platform);
+
+  // Prepare the recordbatch
+  context->queueRecordBatch(recordbatch);
 
   // Performance time open
   auto t1 = perf_clock::now();
-
-  // Prepare the colummn buffers
-  platform->prepare_column_chunks(table->column(0));
-
+  context->enable();
   // Performance timer close
   auto t2 = perf_clock::now();
-  chrono::duration<double> time_span = chrono::duration_cast<
-                                          chrono::duration<double> >(t2 - t1);
-  std::cout << "FPGA copy time: " << time_span.count() << " seconds" << endl;
-
+  std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double> >(t2 - t1);
+  std::cout << "FPGA copy time: " << time_span.count() << " seconds" << std::endl;
 
   // Create a UserCore
-  fletcher::UserCore uc(static_pointer_cast<fletcher::FPGAPlatform>(platform));
+  fletcher::UserCore uc(platform);
 
   // Reset it
   uc.reset();
 
   // Determine size of table
-  fletcher::fr_t last_index = table->num_rows();
-  uc.set_range(0, last_index);
+  int32_t last_index = recordbatch->num_rows();
 
-  // Read back registers for debugging
-  for (int i = 0; i < 6; i++)
-  {
-    fletcher::fr_t reg;
-    platform->read_mmio(i, &reg);
-    cout << "fpga register " << i << ": " << std::hex << reg << endl;
-  }
+  uc.setRange(0, last_index);
 
   // Performance timer open
   t1 = perf_clock::now();
 
   // Start the FPGA user function
   uc.start();
-  uc.wait_for_finish(1000); // Poll every 1 ms
+  uc.waitForFinish(1000); // Poll every 1 ms
 
   // Get the sum from the UserCore
-  fletcher::fa_t sum_fpga = uc.get_return();
+  dau_t ret;
+  uc.getReturn(&ret.lo, &ret.hi);
 
   // Performance timer close
   t2 = perf_clock::now();
-  time_span = chrono::duration_cast<chrono::duration<double>>(t2 - t1);
-  std::cout << "Sum FPGA time: " << time_span.count() << " seconds" << endl;
+  time_span = std::chrono::duration_cast<std::chrono::duration<double >>(t2 - t1);
+  std::cout << "Sum FPGA time: " << time_span.count() << " seconds" << std::endl;
 
-  return sum_fpga;
+  return ret.full;
 }
-
 
 /**
  * Main function for the summing example.
  * Generates list of numbers, sums them on CPU and on FPGA.
  * Finally compares the results.
  */
-int main(int argc, char ** argv)
-{
+int main(int argc, char **argv) {
   unsigned int num_rows = 1024;
 
   if (argc >= 2) {
-    sscanf(argv[1], "%u", &num_rows);
+    std::strtoul(argv[1], nullptr, 10);
   }
   if (num_rows > INT_MAX) {
-    cout << "Too many rows" << endl;
+    std::cout << "Too many rows" << std::endl;
     return EXIT_FAILURE;
   }
 
   // Create table of random numbers
-  shared_ptr<arrow::Table> table = create_table(num_rows);
+  shared_ptr<arrow::RecordBatch> recordbatch = createRecordBatch(num_rows);
 
   // Sum on CPU
-  long long int sum_cpu = arrow_column_sum_cpu(table);
+  int64_t sum_cpu = sumCPU(recordbatch);
 
   // Sum on FPGA
-  fletcher::fa_t sum_fpga = arrow_column_sum_fpga(table);
+  int64_t sum_fpga = sumFPGA(recordbatch);
 
-  cout << "expected sum: " << sum_cpu << endl;
-  cout << "fpga sum: " << sum_fpga << endl;
+  std::cout << "expected sum: " << sum_cpu << std::endl;
+  std::cout << "fpga sum: " << sum_fpga << std::endl;
+
   // Check whether sums are the same
   int exit_status;
   if ((uintmax_t) sum_fpga == (uintmax_t) sum_cpu) {
-    cout << "PASS" << endl;
+    std::cout << "PASS" << std::endl;
     exit_status = EXIT_SUCCESS;
   } else {
-    cout << "ERROR" << endl;
+    std::cout << "ERROR" << std::endl;
     exit_status = EXIT_FAILURE;
   }
   return exit_status;
