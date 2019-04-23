2711a7621a1be0cee250c6bffb674d09b3e2396e
Johan Peltenburg <j.w.peltenburg@tudelft.nl>
2019-04-23 15:27:00 +0200

    Revert "Revert "Move component test to their specific sub library""
    This reverts commit fc4810d9c3b1071a0355ee817f415b377d63d85c.

--- hardware/axi/axi_mmio.tcl
+++ hardware/axi/axi_mmio.tcl
@@ -4,7 +4,7 @@ source $::env(FLETCHER_HARDWARE_DIR)/test/utils.tcl
 add_source $::env(FLETCHER_HARDWARE_DIR)/vhdl/utils/Utils.vhd
 add_source axi.vhd
 add_source axi_mmio.vhd
-add_source axi_mmio_tb.vhd
+add_source test/axi_mmio_tb.vhd
 
 compile_sources
 
