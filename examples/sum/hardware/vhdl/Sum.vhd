484359198bbc7ed8f17f0aa50042bbe129bbd155
Joost Hoozemans <jjhoozemans@tud211698.ws.tudelft.net>
2020-01-31 19:26:09 +0100

    Changes process sensitivity list to old-school VHDL because of oc-accel
    The oc-accel framework uses export_simulation tcl command in Vivado to generate a simulation script.
    This script does not add the required flag for VHDL2008 support to the xvhdl command.

--- examples/sum/hardware/vhdl/Sum.vhd
+++ examples/sum/hardware/vhdl/Sum.vhd
@@ -80,7 +80,9 @@ begin
   -- (registers).
   
   -- Combinatorial part:
-  combinatorial_proc : process (all) is 
+  combinatorial_proc : process (ExampleBatch_number, ExampleBatch_number_last, 
+ExampleBatch_number_valid, accumulator, ExampleBatch_firstIdx, 
+ExampleBatch_lastIdx, state) is 
   begin
     
     -- We first determine the default outputs of our combinatorial circuit.
