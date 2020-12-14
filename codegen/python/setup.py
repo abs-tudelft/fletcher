24b39ce295f930d6f21200eb28cc11061520479d
Matthijs Brobbel <m1brobbel@gmail.com>
2020-12-14 12:56:32 +0100

    Release 0.0.15

--- codegen/python/setup.py
+++ codegen/python/setup.py
@@ -97,7 +97,7 @@ class egg_info(_egg_info):
 
 setup(
     name="pyfletchgen",
-    version="0.0.14",
+    version="0.0.15",
     author="Accelerated Big Data Systems, Delft University of Technology",
     packages=find_packages(),
     url="https://github.com/abs-tudelft/fletcher",
