974a441b4ae897c5f429aca29f3f9cacbd4e6a15
Jeroen van Straten <jeroen.van.straten@gmail.com>
2021-01-06 16:35:22 +0100

    Add write response channel to AXI write converter

--- hardware/axi/Axi_pkg.vhd
+++ hardware/axi/Axi_pkg.vhd
@@ -123,38 +123,48 @@ package Axi_pkg is
       ENABLE_FIFO               : boolean := true;
       SLV_REQ_SLICE_DEPTH       : natural := 2;
       SLV_DAT_SLICE_DEPTH       : natural := 2;
+      SLV_REP_SLICE_DEPTH       : natural := 2;
       MST_REQ_SLICE_DEPTH       : natural := 2;
-      MST_DAT_SLICE_DEPTH       : natural := 2
-    );                          
-    port (                      
+      MST_DAT_SLICE_DEPTH       : natural := 2;
+      MST_REP_SLICE_DEPTH       : natural := 2
+    );
+    port (
       clk                       : in  std_logic;
       reset_n                   : in  std_logic;
       slv_bus_wreq_valid        : in  std_logic;
       slv_bus_wreq_ready        : out std_logic;
       slv_bus_wreq_addr         : in  std_logic_vector(ADDR_WIDTH-1 downto 0);
       slv_bus_wreq_len          : in  std_logic_vector(SLAVE_LEN_WIDTH-1 downto 0);
+      slv_bus_wreq_last         : in  std_logic;
       slv_bus_wdat_valid        : in  std_logic;
       slv_bus_wdat_ready        : out std_logic;
       slv_bus_wdat_data         : in  std_logic_vector(SLAVE_DATA_WIDTH-1 downto 0);
       slv_bus_wdat_strobe       : in  std_logic_vector(SLAVE_DATA_WIDTH/8-1 downto 0);
       slv_bus_wdat_last         : in  std_logic;
+      slv_bus_wrep_valid        : out std_logic;
+      slv_bus_wrep_ready        : in  std_logic;
+      slv_bus_wrep_ok           : out std_logic;
       m_axi_awaddr              : out std_logic_vector(ADDR_WIDTH-1 downto 0);
       m_axi_awlen               : out std_logic_vector(MASTER_LEN_WIDTH-1 downto 0);
       m_axi_awvalid             : out std_logic;
       m_axi_awready             : in  std_logic;
       m_axi_awsize              : out std_logic_vector(2 downto 0);
+      m_axi_awuser              : out std_logic_vector(0 downto 0);
       m_axi_wvalid              : out std_logic;
       m_axi_wready              : in  std_logic;
       m_axi_wdata               : out std_logic_vector(MASTER_DATA_WIDTH-1 downto 0);
       m_axi_wstrb               : out std_logic_vector(MASTER_DATA_WIDTH/8-1 downto 0);
-      m_axi_wlast               : out std_logic
+      m_axi_wlast               : out std_logic;
+      m_axi_bvalid              : in  std_logic;
+      m_axi_bready              : out std_logic;
+      m_axi_bresp               : in  std_logic_vector(1 downto 0)
     );
   end component;
 
   component AxiMmio is
     generic (
       BUS_ADDR_WIDTH            : natural := 32;
-      BUS_DATA_WIDTH            : natural := 32;   
+      BUS_DATA_WIDTH            : natural := 32;
       NUM_REGS                  : natural;
       REG_CONFIG                : string  := "";
       REG_RESET                 : string  := "";
