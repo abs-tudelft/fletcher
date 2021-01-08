35c2df16883b0f23ee2a04ad9a5aaa1606282778
Matthijs Brobbel <m1brobbel@gmail.com>
2021-01-08 19:38:37 +0100

    Bump version to 0.0.17

--- codegen/python/setup.py
+++ codegen/python/setup.py
@@ -97,7 +97,7 @@ class egg_info(_egg_info):
 
 setup(
     name="pyfletchgen",
-    version="0.0.16",
+    version="0.0.17",
     author="Accelerated Big Data Systems, Delft University of Technology",
     packages=find_packages(),
     url="https://github.com/abs-tudelft/fletcher",
